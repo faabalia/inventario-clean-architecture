7a4bf654dd0eb5a2333c6f61bc9ae3409047bbd8
faabalia <faabalia@gmail.com>
2026-06-14 20:26:35 +0200

    feat: initial commit con estructura base de backend Spring Boot

---
+++ README.md
@@ -0,0 +1,1 @@
+# inventario-clean-architecture
---
+++ backend/src/test/java/com/master/inventario/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
